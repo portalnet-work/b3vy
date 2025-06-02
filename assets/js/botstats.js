1dc97c1f255bce2937c65d4c7d43de54bee66b41
Robert Wolff <robert@thedeveco.com>
2025-06-01 18:15:31 -0700

    Leverage portalnet.work as website template
    Signed-off-by: Robert Wolff <robert@portalnet.work>

---
+++ assets/js/botstats.js
@@ -0,0 +1,50 @@
+async function fetchBotStats() {
+  try {
+    const response = await fetch(getBackendURL() + "/stats");
+    const data = await response.json();
+    document.getElementById("totalServers").textContent =
+      data.currentGuildCount.toLocaleString();
+    document.getElementById("comptotalServers").textContent = formatServerCount(
+      data.currentGuildCount
+    );
+    document.getElementById("totalUsers").textContent =
+      data.totalUserCount.toLocaleString();
+    document.getElementById("comptotalUsers").textContent = formatUserCount(
+      data.totalUserCount
+    );
+    document.getElementById("totalNetwork").textContent =
+      data.networkServersCount.toLocaleString();
+    //document.getElementById("comptotalNetwork").textContent = formatServerCount(
+    //  data.networkServersCount
+    // );
+    document.getElementById("totalMessages").textContent =
+      data.messagecount.toLocaleString();
+    document.getElementById("comptotalMessages").textContent =
+      formatMessageCount(data.messagecount);
+  } catch (error) {
+    console.error("Failed to fetch bot stats:", error);
+  }
+}
+
+function formatServerCount(count) {
+  if (count >= 100) {
+    return Math.floor(count / 10) * 10 + "+";
+  }
+  return count.toLocaleString();
+}
+
+function formatUserCount(count) {
+  if (count >= 1000) {
+    return (count / 1000).toFixed(1) + "k+";
+  }
+  return count.toLocaleString();
+}
+
+function formatMessageCount(count) {
+  if (count >= 1000) {
+    return (count / 1000).toFixed(1) + "k+";
+  }
+  return count.toLocaleString();
+}
+
+window.onload = fetchBotStats;
